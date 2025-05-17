1d502702db65c3c82899b70aec20d6eca8c4930a
Gerard Braad <me@gbraad.nl>
2025-05-17 23:50:10 +0800

    Update devenv.nix

--- .idx/devenv.nix
+++ .idx/devenv.nix
@@ -1,29 +1,41 @@
 { pkgs, ... }: {
+  
   channel = "stable-23.11";
-
   packages = [
     pkgs.python311
     pkgs.python311Packages.pip
   ];
 
   env = {};
-  idx = {
 
-    extensions = [
-      "ms-toolsai.jupyter"
-      "ms-python.python"
-    ];
-    
+  idx = {
+      extensions = [
+        "ms-python.debugpy"
+        "ms-python.python"
+        "ms-toolsai.jupyter"
+        "ms-toolsai.jupyter-keymap"
+        "ms-toolsai.jupyter-renderers"
+        "ms-toolsai.vscode-jupyter-cell-tags"
+        "ms-toolsai.vscode-jupyter-slideshow"
+      ];
+
+    previews = {
+      enable = true;
+      previews = {
+      };
+    };
+
     workspace = {
       onCreate = {
         create-venv = ''
           python -m venv .venv
           source .venv/bin/activate
-          pip install -r requirements.txt
+          pip install ipykernel
         '';
       };
+      onStart = {
+      };
     };
-
-    previews = {};
   };
+  
 }
